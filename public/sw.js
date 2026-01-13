953a6ae1049c2ba8697ca9a8bcdd5e630c3c6fa7
haizai007 <haizai007@gmail.com>
2026-01-13 10:19:21 +0800

    add monetag sw and verify

--- public/sw.js
+++ public/sw.js
@@ -16,3 +16,11 @@ self.addEventListener('fetch', (event) => {
     // Simple pass-through for now to avoid errors
     // In a real PWA, caching logic would go here
 });
+
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10455279
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
