664caa2482fa53d0ac5d61aac2347fd68716a383
haizai007 <haizai007@gmail.com>
2026-01-13 11:13:56 +0800

    add ad sw.js and verify

--- public/sw.js
+++ public/sw.js
@@ -24,3 +24,10 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10455302
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
