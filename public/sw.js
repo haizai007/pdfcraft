dd21e230b16b9cbb7642e0f0ad71f1b038030dbb
haizai007 <haizai007@gmail.com>
2026-01-16 16:19:38 +0800

    cancel 10455302
    Comment out options and importScripts for 5gvci.com

--- public/sw.js
+++ public/sw.js
@@ -25,9 +25,11 @@ self.options = {
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10455302
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+/**
+*self.options = {
+*    "domain": "5gvci.com",
+*    "zoneId": 10455302
+*}
+*self.lary = ""
+*importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+*/
